4c592133f47cc62a917110919bec5b65ce089b65
luigi-giordano <luigi.giordano1992@gmail.com>
2024-11-18 10:42:50 +0100

    2 exercise bonus completed

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -16,6 +16,13 @@ console.log(reversedTeachers);
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = [];
+for(let i = 0; i < teachers.length; i++){
+  const name = teachers[i];
+  if(name.length >= 5{
+    longNames.push(name);
+  })
+}
+console.log(longNames);
 
  
 // 3. Rimuovi 'Ed' dall'array teachers
